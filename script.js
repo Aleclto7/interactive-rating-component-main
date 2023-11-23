15cbe59845c3eeae55cc594d737bdff046f06dde
Alexis Calixto <alexiscfx1@gmail.com>
2023-11-23 12:12:58 -0300

    refactoring JavaScript code

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const container1 = document.querySelector('.container__1') 
 const container2 = document.querySelector('.container__2') 
-const option1 = document.getElementById('rating-1')
-const option2 = document.getElementById('rating-2')
-const option3 = document.getElementById('rating-3')
-const option4 = document.getElementById('rating-4')
-const option5 = document.getElementById('rating-5')
+const options = document.getElementsByName('rating')
 const infoSelection = document.querySelector('.container__selection')
 const button = document.querySelector('.container__button')
 
@@ -18,22 +14,10 @@ function activate () {
     }
 }
 function printSelec () {
-    switch (true) {
-        case option1.checked:
-            result = 1
-        break;
-        case option2.checked:
-            result = 2
-        break;
-        case option3.checked:
-            result = 3
-        break;
-        case option4.checked:
-            result = 4
-        break;
-        case option5.checked:
-            result = 5
-        break;
+options.forEach(element => {
+    if (element.checked) {
+        result = element.nextElementSibling.children[0].textContent
     }
+});
     infoSelection.innerHTML = `You selected ${result} out of 5`
 }
